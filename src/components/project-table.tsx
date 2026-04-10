12a460621563c41373acf6cf3f63973f55fe2189
Mrid22 <mridulaga@outlook.com>
2026-04-10 16:05:45 +0300

    centered the table

--- src/components/project-table.tsx
+++ src/components/project-table.tsx
@@ -18,25 +18,27 @@ interface ProjectTableProps {
 
 export function ProjectTable({ projects }: ProjectTableProps) {
   return (
-    <div className="w-full p-4 sm:p-6">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Project Name</TableHead>
-            <TableHead>Path</TableHead>
-            <TableHead>Date Created</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {projects.map((project) => (
-            <TableRow key={project.id} data-testid={`project-row-${project.id}`}>
-              <TableCell className="font-medium">{project.name}</TableCell>
-              <TableCell title={project.path}>{formatPathSuffix(project.path)}</TableCell>
-              <TableCell>{formatDateYYYYMMDD(project.createdAt)}</TableCell>
+    <div className="min-h-screen flex items-center">
+      <div className="w-full p-4 sm:p-6">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Project Name</TableHead>
+              <TableHead>Path</TableHead>
+              <TableHead>Date Created</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {projects.map((project) => (
+              <TableRow key={project.id} data-testid={`project-row-${project.id}`}>
+                <TableCell className="font-medium">{project.name}</TableCell>
+                <TableCell title={project.path}>{formatPathSuffix(project.path)}</TableCell>
+                <TableCell>{formatDateYYYYMMDD(project.createdAt)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   )
 }
